28def17dd7b33bad3d1bb3817708fca6afc1ac7c
Jared Kobos <jaredkobos@gmail.com>
2018-05-08 16:43:17 -0400

    Refactor binary search

--- javascript/binary_search/binarySearch.js
+++ javascript/binary_search/binarySearch.js
@@ -5,18 +5,15 @@ class BinarySearch {
     }
 
     search(target) {
-        let m = Math.floor(this.arr.length / 2);
-        let left = 0;
-        let right = this.arr.length;
+        let m, left = 0, right = this.arr.length;
         while (left <= right) {
             m = Math.floor((right + left) / 2);
-            if (this.arr[m] < target) left = m+1;
-            else if (this.arr[m] > target) right = m -1;
+            if (this.arr[m] < target) left = m + 1;
+            else if (this.arr[m] > target) right = m - 1;
             else return m;
         }
         return false;
     }
 }
-
 
 export default BinarySearch;
