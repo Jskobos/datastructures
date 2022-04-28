f9eeeb794674955f62867e4055d4a4faa983597f
Jared Kobos <jared.kobos@zendesk.com>
2022-04-28 14:26:42 -0400

    Get stuff

--- javascript/binary_search_tree/binarySearchTree.test.ts
+++ javascript/binary_search_tree/binarySearchTree.test.ts
@@ -1,11 +1,25 @@
-import { BinarySearchTree } from './binarySearchTree';
+import { BinarySearchTree } from "./binarySearchTree";
 
 describe("BinarySearchTree", () => {
   it("should create a BST", () => {
     const tree = new BinarySearchTree<number>();
-    tree.put(3);
-    expect(tree.root!.value).toBe(3);
-    tree.put(4);
-    console.log(tree);
-  })
+    expect(tree.root).toBeNull();
+  });
+
+  it("should add items to a BST", () => {
+    const tree = new BinarySearchTree<number>();
+    tree.put(5);
+    expect(tree.root!.value).toBe(5);
+  });
+
+  it("should build the tree correctly", () => {
+    const tree = new BinarySearchTree<number>();
+    [5, 6, 4, 2, 8, 6, 11].forEach((value) => tree.put(value));
+  });
+
+  it("should retrieve a value if it exists in the tree", () => {
+    const tree = new BinarySearchTree<number>();
+    [5, 6, 4, 2, 8, 6, 11].forEach((value) => tree.put(value));
+    expect(tree.get(6)).toBe(6);
+  });
 });
--- javascript/binary_search_tree/binarySearchTree.ts
+++ javascript/binary_search_tree/binarySearchTree.ts
@@ -8,9 +8,9 @@ const newNode = <T>(value: T): Node<T> => {
   return {
     value,
     left: null,
-    right: null
-  }
-}
+    right: null,
+  };
+};
 
 export class BinarySearchTree<T> {
   root: Node<T> | null;
@@ -20,24 +20,32 @@ export class BinarySearchTree<T> {
   }
 
   get(value: T) {
-
+    return this._get(value, this.root);
+  }
+  _get(value: T, node: Node<T> | null): T | null {
+    if (node === null) {
+      return null;
+    } else if (value > node.value) {
+      return this._get(value, node.right);
+    } else if (value < node.value) {
+      return this._get(value, node.left);
+    } else {
+      return node.value;
+    }
   }
 
   put(value: T) {
-    this.root = this._put(this.root, value)
+    this.root = this._put(this.root, value);
   }
 
   _put(node: Node<T> | null, value: T): Node<T> {
     if (node === null) {
       return newNode<T>(value);
-    }
-    else if (value > node.value) {
+    } else if (value > node.value) {
       node.right = this._put(node.right, value);
-    }
-    else if (value < node.value) {
-      node.left = this._put(node.left, value)
-    }
-    else {
+    } else if (value < node.value) {
+      node.left = this._put(node.left, value);
+    } else {
       node.value = value;
     }
     return node;
